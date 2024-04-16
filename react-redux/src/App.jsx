2f34f69bae3243ac6d531d7656226992bd6cf777
EFTEKHER <eftekherali2000gmail.com>
2024-04-16 02:13:13 -0700

    done

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    
+    <div className='h-40 w-40 rounded-full bg-gradient-to-t from-pink-500 to-violet-600 animate-spin shadow-xl p-auto'><h1>Mahi Autisic</h1></div>
     </>
   )
 }
--- react-redux/src/redux/todo/todoSlice.js
+++ react-redux/src/redux/todo/todoSlice.js
@@ -2,4 +2,39 @@ import { createSlice,nanoid } from "@reduxjs/toolkit";
 
 
 
-const initialState = {  
+const initialState = { 
+
+    todos:[{id:1, text:"Hello"}]
+ }
+//reduce functionality
+
+ const todoSlice=createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+      addTodo:(state,action)=>{
+        const todo={id:nanoid() , text:action.payload}
+        state.todos.push(todo)
+
+      } , 
+      removeTodo:(state,action)=>{
+            state.todos=state.todos.filter(todo=>todo.id!==action.payload.id)
+      },
+      updateTodo:(state,action)=>{
+        const {id,newtext}=action.payload;
+        const updateTodo=state.todos.find(todo=>todo.id===id);
+        if(updateTodo){
+            updateTodo.text=newtext;
+        }
+              },
+
+        deleteTodo:(state,action)=>{
+            state.todos=[]
+        }      
+
+      
+    }
+ })
+
+ export const {addTodo,removeTodo,updateTodo,deleteTodo}=todoSlice.actions;
+ export default todoSlice.reducer;
